a2fd66ff5f7c098d5f00d6f7591c88c2c6c66925
Minh Nguyen <nmmyprograms@gmail.com>
2018-07-26 10:28:40 +0700

    Added availability + username into PAUser contract

--- ethereum/contracts/PAUser.sol
+++ ethereum/contracts/PAUser.sol
@@ -7,15 +7,22 @@ contract PAUser is PAUserI, Ownable {
     struct User {
         bytes32 publicKeyLeft;
         bytes32 publicKeyRight;
+        bytes32 username; // Username is unique
         bytes32 name;
         bytes32 avatarUrl;
+        bool available;
         bool isRegistered;
     }
 
     mapping(address => User) users;
+    mapping(bytes32 => bool) usernameUseds;
 
     function isRegistered(address addr) public view returns(bool) {
         return users[addr].isRegistered;
+    }
+
+    function isAvailable(address addr) public view returns(bool) {
+        return users[addr].available;
     }
 
     function getUser(address addr) public view returns(bytes32, bytes32, bytes32, bytes32) {
@@ -23,22 +30,52 @@ contract PAUser is PAUserI, Ownable {
         return (u.publicKeyLeft, u.publicKeyRight, u.name, u.avatarUrl);
     }
 
-    function register(address sender, bytes32 publicKeyLeft, bytes32 publicKeyRight, bytes32 name, bytes32 avatarUrl) public onlyOwner {
+    function register(address sender, bytes32 publicKeyLeft, bytes32 publicKeyRight, bytes32 username, bytes32 name, bytes32 avatarUrl) public onlyOwner {
         require(users[sender].isRegistered == false);
+        require(usernameUseds[username] == false);
 
-        User memory u = User(publicKeyLeft, publicKeyRight, name, avatarUrl, true);
+        User memory u = User(publicKeyLeft, publicKeyRight, username, name, avatarUrl, true, true);
         users[sender] = u;
+        usernameUseds[lowerCase(username)] = true;
 
         emit UserJoined(sender, publicKeyLeft, publicKeyRight, name, avatarUrl);
     }
 
-    function updateProfile(address sender, bytes32 name, bytes32 avatarUrl) public onlyOwner {
+    function updateProfile(address sender, bytes32 username, bytes32 name, bytes32 avatarUrl) public onlyOwner {
         require(users[sender].isRegistered == true);
         
         User storage u = users[sender];
+        if (lowerCase(u.username) != lowerCase(username)) {
+            usernameUseds[u.username] = false;
+            u.username = username;
+            usernameUseds[u.username] = true;
+        }
         u.name = name;
         u.avatarUrl = avatarUrl;
 
         emit UserProfileUpdated(sender, name, avatarUrl);
+    }
+
+    function updateAvailability(address sender, bool availability) external {
+        require(users[sender].isRegistered == true);
+        require(users[sender].available != availability);
+
+        users[sender].available = availability;
+
+        emit UserAvailabilityUpdated(sender, availability);
+    }
+
+    function isUsernameAvailable(bytes32 username) public returns(bool) {
+        return usernameUseds[lowerCase(username)];
+    }
+
+    function lowerCase(bytes32 value) private pure returns (bytes32) {
+        bytes32 result = value;
+        for (uint i = 0; i < 32; i++) {
+            if (uint(value[i]) >= 65 && uint(value[i]) <= 90) {
+                result |= bytes32(0x20) << (31-i)*8;
+            }
+        }
+        return result;
     }
 }
--- ethereum/contracts/PAUserI.sol
+++ ethereum/contracts/PAUserI.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.4.24;
 interface PAUserI {
     event UserJoined(address indexed owner, bytes32 publicKeyLeft, bytes32 publicKeyRight, bytes32 name, bytes32 avatarUrl);
     event UserProfileUpdated(address indexed owner, bytes32 name, bytes32 avatarUrl);
+    event UserAvailabilityUpdated(address indexed owner, bool availability);
 
     function isRegistered(address addr) external view returns(bool);
+    function isAvailable(address addr) external view returns(bool);
     function getUser(address addr) external view returns(bytes32, bytes32, bytes32, bytes32);
-    function register(address sender, bytes32 publicKeyLeft, bytes32 publicKeyRight, bytes32 name, bytes32 avatarUrl) external;
-    function updateProfile(address sender, bytes32 name, bytes32 avatarUrl) external;
+    function register(address sender, bytes32 publicKeyLeft, bytes32 publicKeyRight, bytes32 username, bytes32 name, bytes32 avatarUrl) external;
+    function updateProfile(address sender, bytes32 username, bytes32 name, bytes32 avatarUrl) external;
+    function isUsernameAvailable(bytes32 username) external returns(bool);
+    function updateAvailability(address sender, bool availability) external;
 }
