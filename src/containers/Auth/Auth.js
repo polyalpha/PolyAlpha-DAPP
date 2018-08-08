f44af482603fef553f4faea476aeee5ce704ae0e
Minh Nguyen <nmmyprograms@gmail.com>
2018-08-08 16:28:38 +0700

    Convert Auth object to class

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -9,72 +9,74 @@ import Textarea from 'react-validation/build/textarea';
 import {Button} from './Button';
 
 
-const Auth = (props) => {
+class Auth extends Component {
 
-	let pp = ["Welcome to the next generation decentralised, private and scaleable instant messenger that pays you ABT tokens for your attention.", "When you sign up you will recieve a PolyAlpha messenger address pair on the Ethereum Testnet. If you already have an account, log in with your private key."];
+	constructor(props) {
+		super(props);
+		this.pp = ["Welcome to the next generation decentralised, private and scaleable instant messenger that pays you ABT tokens for your attention.", "When you sign up you will recieve a PolyAlpha messenger address pair on the Ethereum Testnet. If you already have an account, log in with your private key."];
+		this.state = {privateKey: ""};
+	}
 
-	const signin = (
-		<Button icon="svg-crown" className="button catamaran">
-			I want my bids, log me in
-		</Button>
-	);
+	keyValidator = () => {
+		if (!/^[\da-z]{64}$/.test(this.state.privateKey.toString().trim())) {
+			return <div>Bad format secret key</div>;
+		}
+	};
 
-	return (
-		<Fragment>
-			<MainTitle>{props.route.title}</MainTitle>
-			<div id="body-index">
-				<div className="info catamaran">{pp.map((x, i)=><p key={i}>{x}</p>)}</div>
-				<div className="buttons-block">
-					<div className="signup row">
-						<Link to="/auth/signup">
-							<DivButton icon="svg-lightning"  className="button catamaran">
-								I'm new, create an address pair for me
-							</DivButton>
-						</Link>
+	signinHandler = (e) => {
+		e.preventDefault();
+		history.push("/chat/discover")
+	};
+
+	render() {
+		const signin = (
+			<Button icon="svg-crown" className="button catamaran">
+				I want my bids, log me in
+			</Button>
+		);
+
+		return (
+			<Fragment>
+				<MainTitle>{this.props.route.title}</MainTitle>
+				<div id="body-index">
+					<div className="info catamaran">{this.pp.map((x, i)=><p key={i}>{x}</p>)}</div>
+					<div className="buttons-block">
+						<div className="signup row">
+							<Link to="/auth/signup">
+								<DivButton icon="svg-lightning"  className="button catamaran">
+									I'm new, create an address pair for me
+								</DivButton>
+							</Link>
+						</div>
+						<Form className="signin row" method="POST" onSubmit={this.signinHandler}>
+							{this.props.route.path === "/auth/signin" && (
+								<Fragment>
+									<Textarea
+										autoFocus
+										placeholder="Enter your private key"
+										type="text"
+										name="key"
+										className="private-key catamaran"
+										validations={[this.keyValidator]}
+										value={this.privateKey}
+										onChange={(e) => this.setState({privateKey: e.target.value})}
+										rows={1}
+									/>
+									{signin}
+								</Fragment>
+
+							) || (
+								<Fragment>
+									<Link to="/auth/signin">{signin}</Link>
+								</Fragment>
+							)}
+						</Form>
 					</div>
-					<Form className="signin row" method="POST" onSubmit={signinHandler}>
-						{props.route.path === "/auth/signin" && (
-							<Fragment>
-								<Textarea
-									autoFocus
-									placeholder="Enter your private key"
-									type="text"
-									name="key"
-									className="private-key catamaran"
-									validations={[keyValidator]}
-									value="9792f5415b9848c500548328bfef34e0ca6f3604e67eaf33f3295f0465221ee7"
-									rows={1}
-								/>
-								{signin}
-							</Fragment>
-
-						) || (
-							<Fragment>
-								<Link to="/auth/signin">{signin}</Link>
-							</Fragment>
-						)}
-
-					</Form>
 				</div>
-			</div>
-		</Fragment>
-	)
-};
-
-
-const signinHandler = (e) => {
-	e.preventDefault();
-	history.push("/chat/discover")
-
-};
-
-
-const keyValidator = (value) => {
-	if (!/^[\da-z]{64}$/.test(value.toString().trim())) {
-		return <div>Bad format secret key</div>;
+			</Fragment>
+		)
 	}
 };
-
 
 function mapStateToProps(state) {
 	const { auth } = state;
